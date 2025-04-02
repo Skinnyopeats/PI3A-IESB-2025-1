6441009a7e53d74664153d21ce9433af174d1cae
Vitor_M <vitor.mendes@iesb.edu.br>
2025-04-02 18:48:58 -0300

    Modificações no Visual

---
+++ app/src/main/java/com/example/rotalivre/LoginActivity.kt
@@ -0,0 +1,27 @@
+package com.example.rotalivre
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val email: EditText = findViewById(R.id.etEmail)
+        val password: EditText = findViewById(R.id.etPassword)
+        val loginButton: Button = findViewById(R.id.btnLogin)
+
+        loginButton.setOnClickListener {
+            // TODO: Add real login validation later
+
+            // Navigate to MainActivity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Close LoginActivity so user can't go back to login screen
+        }
+    }
+}
